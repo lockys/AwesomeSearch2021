385186cc7c9c1b4e6be6c29ded527a64d25a45c1
lockys <locky4567@gmail.com>
2022-08-06 14:21:43 +0800

    tiny fixes

--- src/components/AwesomeHome/GoogleAd.js
+++ src/components/AwesomeHome/GoogleAd.js
@@ -8,10 +8,11 @@ export default class GoogleAd extends React.Component {
     render () {
         return (
             <ins className="adsbygoogle"
-                 style={{ display: 'block', textAlign: 'center'}}
+                 style={{ display: 'block', textAlign: 'center', width: '768px'}}
                  data-ad-layout="in-article"
                  data-ad-format="fluid"
                  data-ad-client="ca-pub-1371301044344210"
+                 data-full-width-responsive="true"
                  data-ad-slot="1346194813"></ins>
         );
     }
